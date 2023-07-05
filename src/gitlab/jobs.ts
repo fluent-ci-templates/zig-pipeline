dbe2d293dde1fc753b8e7e286d53c232352dded1
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-07-05 18:51:56 +0000

    add `build` job

--- src/gitlab/jobs.ts
+++ src/gitlab/jobs.ts
@@ -1,3 +1,5 @@
 import { Job } from "https://deno.land/x/fluent_gitlab_ci@v0.3.2/mod.ts";
 
-export const test = new Job().script("zig test src");
+export const test = new Job().script("zig test src/*");
+
+export const build = new Job().script("zig build");
--- src/gitlab/pipeline.ts
+++ src/gitlab/pipeline.ts
@@ -1,9 +1,10 @@
 import { GitlabCI } from "https://deno.land/x/fluent_gitlab_ci@v0.3.2/mod.ts";
-import { test } from "./jobs.ts";
+import { build, test } from "./jobs.ts";
 
 const gitlabci = new GitlabCI()
   .image("homebrew/brew")
   .beforeScript("brew install zig")
-  .addJob("test", test);
+  .addJob("test", test)
+  .addJob("build", build);
 
 export default gitlabci;
