c8d321c1ece97cc2fa070d9ce2fba55aa50f91c6
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-27 19:19:56 +0000

    add example project

--- src/gitlab/jobs.ts
+++
@@ -1,5 +0,0 @@
-import { Job } from "https://deno.land/x/fluent_gitlab_ci@v0.3.2/mod.ts";
-
-export const test = new Job().script("zig test src/*");
-
-export const build = new Job().script("zig build");
